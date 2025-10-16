c0d84b75c056e2870f371dbaf8984ba78e272e31
dharmy05 <dharmy.dev05@gmail.com>
2025-10-16 22:12:59 +0100

    changed the route to /me

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 require_once __DIR__ . '/catFactService.php';
 
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($requestUri === '/me') {
+    header('Content-Type: application/json');
 
 $fact = getCatFact();
 
@@ -16,5 +20,9 @@ $response = [
     "fact" => $fact
 ];
 
-// Return JSON
 echo json_encode($response, JSON_PRETTY_PRINT);
+} else {
+    http_response_code(404);
+    header('Content-Type: application/json');
+    echo json_encode(["status" => "error", "message" => "Not found"]);
+}
